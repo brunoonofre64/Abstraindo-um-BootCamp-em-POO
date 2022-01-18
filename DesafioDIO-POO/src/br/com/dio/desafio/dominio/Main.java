da224db22b7fdb27f63fbb75a4d471de6b3fb6f7
onofre <brunoonofre64@gmail.com>
2022-01-18 07:51:21 -0300

    Concluido o desafio

--- DesafioDIO-POO/src/br/com/dio/desafio/dominio/Main.java
+++ DesafioDIO-POO/src/br/com/dio/desafio/dominio/Main.java
@@ -11,10 +11,11 @@ public class Main {
 		curso1.setDescricao("Descrição curso java");
 		curso1.setCargaHoraria(8);
 		
-		Curso curso2= new Curso();
+		Curso curso2 = new Curso();
 		curso1.setTitulo("Curso JS");
 		curso1.setDescricao("Descrição curso JS");
 		curso1.setCargaHoraria(4);
+		
 		
 		
 		Mentoria mentoria  = new Mentoria();
@@ -22,11 +23,27 @@ public class Main {
 		mentoria.setDescricao("descrição mentoria java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java ");
+		bootcamp.setDescricao("Descricao bootcamp java developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
 		
-		
+		Dev devBruno = new Dev();
+		devBruno.setNome("Bruno");
+		System.out.println("Conteudos inscritos: " + devBruno.getConteudosInscritos());
+		devBruno.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos inscritos: " + devBruno.getConteudosInscritos());
+		System.out.println("Conteudos concluidos: " + devBruno.getConteudosConcluidos());
+		devBruno.progredir();
+		System.out.println("Conteudos concluidos: " + devBruno.getConteudosConcluidos());
+		System.out.println(devBruno.calcularTotalXp());
+		devBruno.progredir();
+		devBruno.progredir();
+		System.out.println(devBruno.calcularTotalXp());
+		System.out.println("Conteudos concluidos: " + devBruno.getConteudosConcluidos());
+		System.out.println("Conteudos inscritos: " + devBruno.getConteudosInscritos());
 		
 		
 	}
